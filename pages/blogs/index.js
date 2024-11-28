f5d583a386b97b6b3ab6cc66bbc00c0eb223ae2a
Mohit Rawat <mohitrawat@Mohits-MacBook-Air.local>
2024-11-28 10:55:10 +0530

    latest

--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -1,65 +1,58 @@
-import React, { useEffect } from 'react'
-import BlogCard from '../../components/blog-card/blogCard'
-import styles from './blogs.module.css'
-import { apiCall } from '../../utils/common'
-const index = ({ blogs,blogsBanner }) => {
+import React, { useEffect } from 'react';
+import BlogCard from '../../components/blog-card/blogCard';
+import styles from './blogs.module.css';
+import { apiCall } from '../../utils/common';
+
+const Index = ({ blogs, blogsBanner }) => {
     const handleScrollParallax = () => {
         const parallaxImage = document.querySelector(`.${styles['parallax-image']}`);
         if (parallaxImage) {
-          const scrollPosition = window.scrollY;
-          parallaxImage.style.transform = `translateY(${scrollPosition * 0.5}px)`; // Adjust speed factor
+            const scrollPosition = window.scrollY;
+            parallaxImage.style.transform = `translateY(${scrollPosition * 0.5}px)`; // Adjust speed factor
         }
-      };
-    
-      useEffect(() => {
+    };
+
+    useEffect(() => {
         window.addEventListener('scroll', handleScrollParallax);
         return () => window.removeEventListener('scroll', handleScrollParallax);
-      }, []);
-     
+    }, []);
+
     return (
         <div>
-             <header className={styles.header}>
-        <div className={styles['parallax-container']}>
-          <img src={blogsBanner.data.bannerUrls[0]} alt="Destination Banner" className={styles['parallax-image']} />
-        </div>
-        <div className={styles.header_content}>
-          <h1 className={styles.title}>Blogs</h1>
-          <nav>Home ➔ Blogs</nav>
-        </div>
-      </header>
+            <header className={styles.header}>
+                <div className={styles['parallax-container']}>
+                    <img src={blogsBanner.data.bannerUrls[0]} alt="Destination Banner" className={styles['parallax-image']} />
+                </div>
+                <div className={styles.header_content}>
+                    <h1 className={styles.title}>Blogs</h1>
+                    <nav>Home ➔ Blogs</nav>
+                </div>
+            </header>
             <div className={styles['blog-grid']}>
-                {blogs.data.map((blogs) => {
-                    return (
-                        <>
-                            <BlogCard key={blogs.id} blogs={blogs} />
-
-                        </>
-                    )
-                })}
-
-
+                {blogs.data.map((blog) => (
+                    <BlogCard key={blog.id} blogs={blog} />
+                ))}
             </div>
         </div>
-    )
+    );
 };
 
-export default index
-export async function getStaticProps() {
+export default Index;
 
+export async function getStaticProps() {
     const blogs = await apiCall({
         endpoint: `/api/getAllBlogs`,
         method: 'GET',
-
     });
     const blogsBanner = await apiCall({
         endpoint: `/api/getBanner?page=blogsBanner`,
         method: 'GET',
-    
-      });
+    });
+
     return {
         props: {
             blogs,
-            blogsBanner
+            blogsBanner,
         },
         revalidate: 600,
     };

1f015fc2f2eba2bc914778b93ccfa57a337e3e94
Mohit Rawat <mohitrawat@Mohits-MacBook-Air.local>
2024-12-11 13:40:29 +0530

    latest

--- components/blogs/blogs.js
+++ components/blogs/blogs.js
@@ -1,20 +1,65 @@
-import React from 'react'
-import styles from './blogs.module.css'
+import React, { useEffect, useState } from 'react';
+import Slider from 'react-slick';
+import styles from './blogs.module.css';
 import BlogCard from '../blog-card/blogCard';
-const blogs = ({blogs}) => {
-    const firstFiveBlogs = blogs.slice(0, 5);
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+const CustomPrevArrow = ({ onClick }) => (
+  <button className={styles.prevArrow} onClick={onClick}>
+    &#8592; {/* Left arrow symbol */}
+  </button>
+);
+
+const CustomNextArrow = ({ onClick }) => (
+  <button className={styles.nextArrow} onClick={onClick}>
+    &#8594;
+  </button>
+);
+
+const Blogs = ({ blogs }) => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768); 
+    };
+
+    handleResize(); // Check on mount
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: <CustomPrevArrow />,
+    nextArrow: <CustomNextArrow />,
+  };
+
   return (
     <>
-    <h1 className={styles.blogsHeading}>Blogs</h1>
-    <div className={styles.blogsContainer}>
-      
-    {blogs.slice(0,6).map((blogs) => (
-      <BlogCard key={blogs.id} blogs={blogs} />
-    ))}
-  </div>
-    
+      <h1 className={styles.blogsHeading}>Blogs</h1>
+      <div className={styles.blogsContainer}>
+        {isMobile ? (
+          <Slider {...settings}>
+            {blogs.slice(0, 6).map((blog) => (
+              <div key={blog.id}>
+                <BlogCard blogs={blog} />
+              </div>
+            ))}
+          </Slider>
+        ) : (
+          blogs.slice(0, 6).map((blog) => (
+            <BlogCard key={blog.id} blogs={blog} />
+          ))
+        )}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default blogs
+export default Blogs;

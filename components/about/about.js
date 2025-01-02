03305ed4572a613681dcf07b82a706e129289096
Mohit Rawat <mohitrawat@Mohits-MacBook-Air.local>
2025-01-02 19:10:10 +0530

    latest

--- components/about/about.js
+++ components/about/about.js
@@ -19,22 +19,22 @@ const about = () => {
                     </div>
                 </div>
                 <p className={styles['description']}>
-                Devsthan Expert Pvt. Ltd. is a premier travel company dedicated to crafting unforgettable journeys that celebrate the vibrant culture, heritage, and natural beauty of India. With a team of experienced travel enthusiasts, we specialize in curating personalized itineraries for both domestic and international travelers. From serene landscapes to bustling cities, our mission is to offer seamless travel experiences that inspire and captivate. Trust us to turn your travel dreams into reality with exceptional service and attention to detail.
+                    Devsthan Expert Pvt. Ltd. is a premier travel company dedicated to crafting unforgettable journeys that celebrate the vibrant culture, heritage, and natural beauty of India. With a team of experienced travel enthusiasts, we specialize in curating personalized itineraries for both domestic and international travelers. From serene landscapes to bustling cities, our mission is to offer seamless travel experiences that inspire and captivate. Trust us to turn your travel dreams into reality with exceptional service and attention to detail.
                 </p>
-              <Link href='/about'><button className={styles['button']}>More About</button></Link>  
+                <Link href='/about'><button className={styles['button']}>More About</button></Link>
             </div>
             <div className={styles['images-container']}>
                 <div className={styles['image-wrapper']}>
-                    <Image src="https://res.cloudinary.com/dmyzudtut/image/upload/v1735376268/images/uxs5pgmanov5vyntgom4.webp" alt="Group" width={200} height={200} className={styles['about-images']}/>
+                    <Image src="https://res.cloudinary.com/dmyzudtut/image/upload/v1735376268/images/uxs5pgmanov5vyntgom4.webp" alt="Group" width={200} height={200} className={styles['about-images']} />
                 </div>
                 <div className={styles['image-wrapper']}>
-                    <Image src="https://res.cloudinary.com/dmyzudtut/image/upload/v1735376398/about_zlvr4e.jpg" alt="Resort" width={200} height={200} className={styles['about-images']}/>
+                    <Image src="https://res.cloudinary.com/dmyzudtut/image/upload/v1735376398/about_zlvr4e.jpg" alt="Resort" width={200} height={200} className={styles['about-images']} />
                 </div>
                 <div className={styles['image-wrapper']}>
-                    <Image src="https://res.cloudinary.com/dmyzudtut/image/upload/v1735376406/about2_yy6k9x.jpg" alt="Desert" width={200} height={200} className={styles['about-images']}/>
+                    <Image src="https://res.cloudinary.com/dmyzudtut/image/upload/v1735376406/about2_yy6k9x.jpg" alt="Desert" width={200} height={200} className={styles['about-images']} />
                 </div>
                 <div className={styles['image-wrapper']}>
-                    <Image src="https://res.cloudinary.com/dmyzudtut/image/upload/v1735377308/about3_qul6sh.jpg" alt="Couple" width={200} height={200} className={styles['about-images']}/>
+                    <Image src="https://res.cloudinary.com/dmyzudtut/image/upload/v1735377308/about3_qul6sh.jpg" alt="Couple" width={200} height={200} className={styles['about-images']} />
                 </div>
             </div>
         </section>
